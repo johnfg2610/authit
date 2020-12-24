5897ef08ba134a1863e0f433a406480366e99da7
John <5445550+johnfg2610@users.noreply.github.com>
2020-12-24 08:24:56 +0000

    Setup google wire

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"os"
 
+	"gocloud.dev/runtimevar"
+	"gocloud.dev/secrets"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/johnfg2610/authit/pkg/password"
 	"gocloud.dev/server"
 	"gocloud.dev/server/requestlog"
 )
+
+type Config struct {
+	GormConnectURL []byte
+}
 
 var (
 	serverOptions = &server.Options{
@@ -17,7 +29,49 @@ var (
 	}
 )
 
+func NewVariable(ctx context.Context) (*runtimevar.Variable, error) {
+	v, err := runtimevar.OpenVariable(ctx, "constant://?val=hello+world&decoder=string")
+	if err != nil {
+		return nil, err
+	}
+	return v, nil
+}
+
+func NewSecrets(ctx context.Context) (*secrets.Keeper, error) {
+	keyKeeper, err := secrets.OpenKeeper(ctx, "base64key://")
+	if err != nil {
+		return nil, err
+	}
+	return keyKeeper, nil
+}
+
+func NewGormDB(ctx context.Context, vari *runtimevar.Variable, secr *secrets.Keeper) (*gorm.DB, error) {
+	snapshot, err := vari.Latest(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	cnf, ok := snapshot.Value.(Config)
+
+	if !ok {
+		return nil, fmt.Errorf("Snapshot value was not of expected type")
+	}
+
+	plain, err := secr.Decrypt(ctx, cnf.GormConnectURL)
+	if err != nil {
+		return nil, err
+	}
+
+	return gorm.Open(sqlite.Open(string(plain)), &gorm.Config{})
+}
+
+func NewPasswordHasher() (password.Hasher, error) {
+	return password.NewBcryptHasher(), nil
+}
+
 func main() {
+	//e, err := InitializeEvent()
+
 	router := chi.NewRouter()
 	srv := server.New(router, serverOptions)
 
